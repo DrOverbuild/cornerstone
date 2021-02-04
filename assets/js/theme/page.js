61d53983deb12b1ba9b313d8e486f285224b85da
Jasper Reddin <jasper@jasperreddin.com>
2021-02-04 12:16:41 -0600

    Responsive aspect ratio for iframes in pages (#27)

--- assets/js/theme/page.js
+++ assets/js/theme/page.js
@@ -2,18 +2,18 @@ import PageManager from './page-manager';
 import $ from 'jquery';
 
 export default class Page extends PageManager {
-    before(next) {
+    onReady() {
+        // hide the heading if page has an image with the page_banner class
         if ($('.page_banner').length) {
-            // if($('.page-content').has('page-banner')) {
-            // if(false) {
             $('.page-heading').hide();
         }
 
-        next();
+        // make iframes responsive and constrained to aspect ratio
+        const iframe = $('.page-content').find('iframe');
+        if (iframe.length != 0) {
+            const aspect169 = $('<div class="aspect-16-9"></div>');
+            aspect169.append(iframe.clone());
+            iframe.replaceWith(aspect169);
+        }
     }
-
-    loaded(next) {
-        next();
-    }
-
 }
